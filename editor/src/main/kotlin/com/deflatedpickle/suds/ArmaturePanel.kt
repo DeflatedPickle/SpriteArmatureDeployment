ac348614e8b0aebbf1a442e01af2ca1a6f04f154
DeflatedPickle <DeflatedPickle@gmail.com>
2019-06-16 18:54:36 +0100

    Created a class for editor bones

--- editor/src/main/kotlin/com/deflatedpickle/suds/ArmaturePanel.kt
+++ editor/src/main/kotlin/com/deflatedpickle/suds/ArmaturePanel.kt
@@ -10,7 +10,8 @@ class ArmaturePanel : JPanel() {
     var frame = 0
 
     var scaleFactor = 10
-    val circleSize = 2 * scaleFactor
+
+    val boneList = mutableListOf<DisplayBone>()
 
     lateinit var centre: Point
 
@@ -22,35 +23,31 @@ class ArmaturePanel : JPanel() {
         super.paintComponent(g)
         val g2D = g as Graphics2D
 
+        if (boneList.isEmpty()) {
+            for (i in armature.frames[frame].values) {
+                loopBones(i, null)
+            }
+        }
+
         val originalFont = g2D.font
         g2D.font = Font(originalFont.fontName, Font.BOLD, 14)
 
         centre = Point(this.width / 2, this.height / 2)
 
-        for (i in armature.frames[frame].values) {
-            loopBones(g2D, i)
+        for (i in boneList) {
+            i.draw(g2D, centre, scaleFactor)
         }
     }
 
-    fun loopBones(g2D: Graphics2D, bone: StaticBone) {
+    fun loopBones(bone: StaticBone, parentBone: DisplayBone?) {
         val children = bone.children
-        val properties = bone.properties
 
-        val circleX = (centre.x - circleSize / 2) + properties["x"] as Int * scaleFactor
-        val circleY = (centre.y - circleSize / 2) + properties["y"] as Int * scaleFactor
+        val displayBone = DisplayBone(bone, parentBone)
+        boneList.add(displayBone)
 
-        val textX = circleX - (g2D.fontMetrics.stringWidth(bone.name) / 4)
-        val textY = circleY + (g2D.fontMetrics.height)
-
-        g2D.color = Color.BLUE
-        g2D.fillOval(circleX, circleY, circleSize, circleSize)
-
-        g2D.color = Color.ORANGE
-        g2D.drawString(bone.name, textX, textY)
-
-        if (bone.children != null) {
+        if (children != null) {
             for (i in bone.children.values) {
-                loopBones(g2D, i)
+                loopBones(i, displayBone)
             }
         }
     }
---
+++ editor/src/main/kotlin/com/deflatedpickle/suds/DisplayBone.kt
@@ -0,0 +1,39 @@
+package com.deflatedpickle.suds
+
+import com.deflatedpickle.sad.StaticBone
+import java.awt.*
+
+class DisplayBone(val bone: StaticBone, var parentBone: DisplayBone?) {
+    var size: Point
+    var location: Point
+
+    init {
+        val properties = bone.properties
+        location = Point(properties["x"] as Int, properties["y"] as Int)
+
+        size = Point(2, 2)
+    }
+
+    fun draw(g2D: Graphics2D, centre: Point, scaleFactor: Int) {
+        val circleX = (centre.x - (size.x * scaleFactor) / 2) + location.x * scaleFactor
+        val circleY = (centre.y - (size.y * scaleFactor) / 2) + location.y * scaleFactor
+
+        val textX = circleX - (g2D.fontMetrics.stringWidth(bone.name) / 4)
+        val textY = circleY + (g2D.fontMetrics.height)
+
+        g2D.color = Color.BLUE
+        g2D.fillOval(circleX, circleY, size.x * scaleFactor, size.y * scaleFactor)
+
+        g2D.color = Color.ORANGE
+        g2D.drawString(bone.name, textX, textY)
+
+        if (parentBone != null) {
+            val parentCircleX = (centre.x - (parentBone!!.size.x * scaleFactor) / 2) + parentBone!!.location.x * scaleFactor
+            val parentCircleY = (centre.y - (parentBone!!.size.y * scaleFactor) / 2) + parentBone!!.location.y * scaleFactor
+
+            g2D.color = Color.GREEN
+            g2D.stroke = BasicStroke(4f)
+            g2D.drawLine(circleX, circleY, parentCircleX, parentCircleY)
+        }
+    }
+}
